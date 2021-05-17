8bcff8d2a9e6b2a8025cd179eabe9423f7160a39
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-17 18:02:45 +0900

    Create sample1.cs

---
+++ lesson10/sample1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Windows.Forms;
+using Sytem.IO;
+
+class Sample1:Form
+{
+  private Button bt;
+  private Label[] lb =new Label[3];
+  [STAThread]
+  public static void Main(){
+    Application.Run(new Sample1());
+  }
+  public Sample1(){
+    this.Text="sample";
+    this.Width
+  }
+
+
+
+
+}
