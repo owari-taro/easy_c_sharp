83d01645a515ddb32f46d590f0e661fd8e0e9e65
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-18 16:35:48 +0900

    Create AbstractBasic.cs

---
+++ AbstractBasic.cs
@@ -0,0 +1,16 @@
+abstract class Figure{
+  public double Width{get;set;}
+  public double Height{get;set;}
+  
+  public Figure(double width,double height){
+    this.Width=width;
+    this.Height=height;
+  
+  }_
+  
+ public abstract double GetArea();
+
+
+
+
+}
