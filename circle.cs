1030cc48ef1e979cd16df2268fab53772c9470f5
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-18 14:40:55 +0900

    Update circle.cs

--- circle.cs
+++ circle.cs
@@ -10,3 +10,29 @@ class Circle{
 
 
 }
+
+
+
+
+class Circle2{
+  double radius;
+  public Circle(double radius=1){
+    this.radius=radius;
+  }
+  
+  public double GetArea(){
+    return this.radius*this.radius*Math.PI;
+  
+  }
+  
+  public double GetAverage(params double[] values ){
+    double sum=0d
+    foreach(var value in values){
+      sum+=value;
+    }
+    return sum/values.Length:
+  
+  }
+
+
+}
