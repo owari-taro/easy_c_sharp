e753b75a972dd5cd13f6a2d4613da6c23b63d19f
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-18 13:47:37 +0900

    Create circle.cs

---
+++ circle.cs
@@ -0,0 +1,12 @@
+class Circle{
+  double radius;
+  public Circle(double radius){
+    this.radius=radius;
+  }
+  public double GetArea(){
+    return this.radius*this.radius*Math.PI;
+  
+  }
+
+
+}
